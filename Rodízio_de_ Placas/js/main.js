292eae8a4f640e768bc8986c50c79de0e59689fb
Isabela Motta <133990688+isamotta30@users.noreply.github.com>
2023-11-23 13:17:57 -0300

    Add files via upload

--- Rodízio_de_ Placas/js/main.js
+++ Rodízio_de_ Placas/js/main.js
@@ -5,33 +5,32 @@ let typeAuto;
 let hora;
 let typeStreet;
 document.getElementById("button").onclick = function () {
-    numSign = document.getElementById("myInput").value;
-    wDay = document.getElementById("myInput2").value;
-   typeAuto = document.getElementById("myInput3").value;
-    hora = document.getElementById("myInput4").value;
-    typeStreet = document.getElementById("myInput5").value;
+    numSign = document.getElementById("placa").value;
+    wDay = document.getElementById("semana").value;
+   typeAuto = document.getElementById("automovel").value;
+    hora = document.getElementById("horario").value;
+    typeStreet = document.getElementById("circulacao").value;
 
     if ((numSign == 1 || numSign == 2) && (wDay == "Segunda-feira") && (typeAuto == "Carro") && ((hora >= 7 && hora <= 10) || (hora >= 17 && hora <= 20)) && (typeStreet == "Áreas vermelhas")) {
         document.getElementById("carros").textContent = `Seu automóvel não poderá passar nessa via com essas informações :(`
     }
 
-    else if ((numSign == 3 || numSign == 4) && (wDay == "Segunda-feira") && (typeAuto == "Carro") && ((hora >= 7 && hora<= 10) || (hora >= 17 && hora <= 20)) && (typeStreet == "Áreas vermelhas")) {
+    else if ((numSign == 3 || numSign == 4) && (wDay == "Terça-feira") && (typeAuto == "Carro") && ((hora >= 7 && hora<= 10) || (hora >= 17 && hora <= 20)) && (typeStreet == "Áreas vermelhas")) {
         document.getElementById("carros").textContent = `Seu automóvel não poderá passar nessa via com essas informações :(`
     }
 
-    else if ((numSign == 5 || numSign == 6) && (wDay == "Segunda-feira") && (typeAuto == "Carro") && ((hora >= 7 && hora <= 10) || (hora >= 17 && hora <= 20)) && (typeStreet == "Áreas vermelhas")) {
+    else if ((numSign == 5 || numSign == 6) && (wDay == "Quarta-feira") && (typeAuto == "Carro") && ((hora >= 7 && hora <= 10) || (hora >= 17 && hora <= 20)) && (typeStreet == "Áreas vermelhas")) {
         document.getElementById("carros").textContent = `Seu automóvel não poderá passar nessa via com essas informações :(`
     }
 
-    else if ((numSign == 7 || numSign == 8) && (wDay == "Segunda-feira") && (typeAuto == "Carro") && ((hora >= 7 && hora<= 10) || (hora>= 17 && hora <= 20)) && (typeStreet == "Áreas vermelhas")) {
+    else if ((numSign == 7 || numSign == 8) && (wDay == "Quinta-feira") && (typeAuto == "Carro") && ((hora >= 7 && hora<= 10) || (hora>= 17 && hora <= 20)) && (typeStreet == "Áreas vermelhas")) {
         document.getElementById("carros").textContent = `Seu automóvel não poderá passar nessa via com essas informações :(`
     }
     
-    else if ((numSign == 9 || numSign == 0) && (wDay == "Segunda-feira") && (typeAuto == "Carro") && ((hora >= 7 && hora <= 10) || (hora >= 17 && hora <= 20)) && (typeStreet == "Áreas vermelhas")) {
+    else if ((numSign == 9 || numSign == 0) && (wDay == "Sexta-feira") && (typeAuto == "Carro") && ((hora >= 7 && hora <= 10) || (hora >= 17 && hora <= 20)) && (typeStreet == "Áreas vermelhas")) {
         document.getElementById("carros").textContent = `Seu automóvel não poderá passar nessa via com essas informações :(`
     }
-
-    else if ((typeAuto == "Caminhão") && ((hour >= 7 && hour <= 10) || (hora>= 17 && hora <= 20)) && (typeStreet == "Áreas vermelhas")) {
+    else if ((typeAuto == "Caminhão") &&  (typeStreet == "Vias limítrofes")) {
         document.getElementById("carros").textContent = ` Seu automóvel não poderá passar nessa via com essas informações :(`
     }
 
